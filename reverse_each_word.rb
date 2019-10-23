5d1b2e4cc6eaa5e13009495a6ce48939a16e83a2
Alex Suthammanont <asuthamm@gmail.com>
2019-10-23 18:24:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(str)
   arr = str.split
   new_arr = []
   
-  newarr = arr.collect { |word| word.reverse }
+  new_arr = arr.collect { |word| word.reverse }
   new_arr.join(" ")
 end
